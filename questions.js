c2c8ca4ea4f78bd47c25f65d8031c764c49127b3
Levi Giraud <giraudlevi@gmail.com>
2019-10-02 17:36:55 -0600

    added a test var to the questions.js

---
+++ questions.js
@@ -0,0 +1,26 @@
+var questions = {
+    "question1": [
+      "yes",
+      "no1",
+      "no2",
+      "no3",
+    ],
+    "question2": [
+        "no1",
+        "no2",
+        "no3",
+        "yes",
+      ],
+      "question3": [
+        "no2",
+        "no3",
+        "yes",
+        "no1",
+      ],
+      "question4": [
+        "no3",
+        "yes",
+        "no1",
+        "no2",
+      ],
+  };
