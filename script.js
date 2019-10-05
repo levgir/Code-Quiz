62946fae158743bbb27b88ee69f2aca03c7cd9af
Levi Giraud <giraudlevi@gmail.com>
2019-10-04 20:43:05 -0600

    added logic, reworked arrays

--- script.js
+++ script.js
@@ -1,81 +1,63 @@
 var questions = [
-    "Inside which HTML element do we put the JavaScript?",
-    "Where is the correct place to insert a JavaScript?",
-    "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    "How do you create a function in JavaScript?",
-    "How to write an IF statement in JavaScript?"
-    ];
-  
-  var answers = {
-    "answer1":"<script>",
-    "answer2":"Both the <head> section and the <body> section ar correct",
-    "answer3":"<script src='xxx.js'>",
-    "answer4":"function myFunction()",
-    "answer5":"if(i == 5)"
-  };
-  
-  var possibleAnswers = {
-    "question1": [
-      "<js>",
-      "<script>",
-      "<javascript>",
-      "<scripting>"
-    ],
-    "question2": [
-      "The <head> section",
-      "Both the <head> section and the <body> section ar correct",
-      "The <body> section",
-      "The <garbage> can"
-    ],
-    "question3": [
-      "<script href='xxx.js'>",
-      "<script name='xxx.js'>",
-      "<script src='xxx.js'>",
-      "<script id='xxx.js'>"
-    ],
-    "question4": [
-      "function = myFunction()",
-      "function myFunction()",
-      "function:myFunction()",
-      "myFunction function()"
-    ],
-    "question5": [
-      "if i = 5",
-      "if(i == 5)",
-      "if i == 5 then",
-      "if i = 5 then"
-    ]
+  { 
+      q: "Inside which HTML element do we put the JavaScript?",
+      a: "<script>",
+      pas: ["<js>", "<script>", "<javascript>", "<scripting>"]
+  },
+  { 
+      q: "Where is the correct place to insert a JavaScript?",
+      a: "Both the <head> section and the <body> section ar correct",
+      pas: ["The <head> section", "Both the <head> section and the <body> section ar correct", "The <body> section", "The <garbage> can"]
+  },
+  {
+      q: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      a: "<script src='xxx.js'>",
+      pas: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script id='xxx.js'>"]
+  },
+  {
+      q: "How do you create a function in JavaScript?",
+      a: "function myFunction()",
+      pas: ["function = myFunction()", "function myFunction()", "function:myFunction()", "myFunction function()"]
+  },
+  { 
+      q: "How to write an IF statement in JavaScript?",
+      a: "if(i == 5)",
+      pas: ["if i = 5", "if(i == 5)", "if i == 5 then", "if i = 5 then"]    
   }
+];
 
+var currentQuestion = 0;
+var timer = 100;
 var startBtn = document.querySelector("#startButton");
 var questionsArea = document.querySelector("#questionsDiv");
+var answer ="";
+var possibleAnswers = [];
 
-startBtn.addEventListener("click", function() {
-    questionsArea.innerHTML = "";
-
-    for (var i = 0; i < questions.lenght; i++) {
-        var newQuestion = document.createElement("<div>");
-        var newText = questions[i];
-        newQuestion.innerHTML("<h1>",newText,"</h1>");
-        console.log(newQuestion);
-        questionsArea.innerHTML = newQuestion;
+function gameTime() {
+  for (var i = 0; i < 4; i++) {
+    var newQuestion = questions[i].q;
+    var newQuestionDiv = document.createElement('div');
+    newQuestionDiv.textContent = newQuestion;
+    questionsArea.appendChild(newQuestionDiv);
+    possibleAnswers = questions[i].pas;
+    answer = questions[i].a;
+    
+    possibleAnswers.forEach(function (possibleAnswer) {
+      var answerBtn = document.createElement('button');
+      var answerText = document.createTextNode(possibleAnswer);
+      answerBtn.appendChild(answerText);
+      questionsArea.append(answerBtn);
       
+    })
 
-    };
+    
+  };
 
-
-
-
-
-
+};
 
 
+startBtn.addEventListener("click", function () {
+  questionsArea.innerHTML = "";
+  gameTime();
+});
 
-
-
-
-
-
-
-
-  });
